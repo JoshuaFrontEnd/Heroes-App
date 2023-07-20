bdf9ec3898ac99a71a2db4c0b7f460390ffb06a6
Joshua <hola@joshua.cl>
2023-07-20 15:25:36 -0400

    Usando 'useMemo' para optimizar la carga de datos declarada dentro de componentes

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react';
 import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers';
 
 export const HeroList = ({ publisher }) => {
 
-  const heroes = getHeroesByPublisher( publisher );
+  // Acá con 'useMemo' cada vez que cambie el 'publisher' se ejecutara el callback 'getheroesByPublisher' y no cuando se cargue el componente
+  const heroes = useMemo( () => getHeroesByPublisher( publisher ), [ publisher ] );
 
   return (
     <div className="row row-col-1 row-cols-md-3 g-3">
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,6 @@
 import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../helpers';
+import { useMemo } from 'react';
 
 export const HeroPage = () => {
 
@@ -7,7 +8,18 @@ export const HeroPage = () => {
 
   const navigate = useNavigate();
 
-  const hero = getHeroById( id );
+  /* ----------------------------------------------------------------
+
+    Cuando declaramos funciones o valores adentro de un 'functional component' React las volvera a renderizar cada vez que se cargue el componente, o cada vez que cambie el estado de los valores del componente, esto puede perjudicar el rendimiento de la aplicación, para resolver esto necesitamos memorizar estos datos.
+
+    Podemos usar 'useMemo' para memorizar los valores y 'useCallback' para memorizar las funciones
+
+    Esto mismo se aplicara en 'HeroList.jsx'
+
+  ---------------------------------------------------------------- */
+
+  // En este caso 'useMemo' va a disparar el callback cada vez que sus dependencias cambien, y no cuando el componente se cargue o se vuelva a renderizar, es decir, solo va a ejecutarse la función 'getHeroById' cuando cambie el 'id'
+  const hero = useMemo( () => getHeroById( id ), [id] );
 
   const onNavigateBack = () => {
     // Acá se pueden usar muchas tecnicas, pero por ahora solo regresaremos a la pagina anterior del historial
