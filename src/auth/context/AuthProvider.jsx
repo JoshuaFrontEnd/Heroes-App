c188a6300b99eb088a9529a26d8cdb39f1f87715
Joshua <hola@joshua.cl>
2023-08-21 15:54:16 -0400

    Mantener el usuario activo aunque se recargue la pagina

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,24 +4,39 @@ import { authReducer } from './authReducer';
 import { types } from '../types/types';
 
 // Creo el estado inicial a usar en 'useReducer'
-const initialState = {
-  logged: false
-};
+// const initialState = {
+//   logged: false
+// };
+
+// Acá obtengo el nombre del usuario si ha sido guardado en el 'localStorage'
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    // Esto es como preguntar ¿Si el usuario existe? (usando doble negacion !!)
+    logged: !!user,
+    user,
+  }
+}
 
 // Acá estoy utilizando la propiedad 'children' de React, para pasar todos los componentes "hijos" al proveedor
 export const AuthProvider = ({ children }) => {
 
   //Manejo el estado del proveedor con el hook 'useReducer' utilizando el reducer creado en 'authReducer.jsx'
-  const [ authState, dispatch ] = useReducer( authReducer, initialState );
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init );
 
   const login = ( name = '' ) => {
+
+    // Genero la estructura del usuario
+    const user = { id: 'ABC', name }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name
-      }
+      payload: user
     }
+
+    //Guardo el usuario en el 'localStorage'
+    localStorage.setItem( 'user', JSON.stringify( user ) );
 
     dispatch( action );
   };
